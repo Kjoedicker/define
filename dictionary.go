84bb7caaf8b687de544d5fbd18ed7a5d4ae2b683
keifer <kjoedicker@gmail.com>
2020-10-10 04:46:35 -0400

    handle dictionary.json files that are empty, to prevent nil map panic

--- dictionary.go
+++ dictionary.go
@@ -11,11 +11,17 @@ func updateDict(dictionary map[string][]string, word string, definition []string
 		dictionary[word] = definition
 		return false
 	}
+
 	return true
 }
 
 func getDict(dictionary string) map[string][]string {
-	return froJSON(grabJSON(dictionary))
+	tmpDict := froJSON(grabJSON(dictionary))
+	if tmpDict != nil {
+		return tmpDict
+	}
+
+	return make(map[string][]string)
 }
 
 func checkDict(word string, dictionary map[string][]string) ([]string, error) {
